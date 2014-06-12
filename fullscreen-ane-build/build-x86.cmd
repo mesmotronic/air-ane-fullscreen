5b004d2f4f64e726db052b6f02d33b64ab538659
Neil Rackett <neil.rackett@mesmotronic.com>
2014-06-12 13:49:37 +0100

    Added support for Android x86

--- fullscreen-ane-build/build-x86.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-adt ^
- -package ^
- -target ane ./AndroidFullScreen.ane extension-x86.xml ^
- -swc ../fullscreen-ane-android/bin/fullscreen-ane-android.swc ^
- -platform Android-ARM -C android . ^
- -platform Android-x86 -C android . ^
- -platform default -C default . 
--- fullscreen-ane-build/build.cmd
+++ fullscreen-ane-build/build.cmd
@@ -1,3 +1,9 @@
 @echo off
 
-adt -package -target ane ./AndroidFullScreen.ane extension.xml -swc ../fullscreen-ane-android/bin/fullscreen-ane-android.swc -platform Android-ARM -C android . -platform default -C default . 
+adt ^
+ -package ^
+ -target ane ./AndroidFullScreen.ane extension.xml ^
+ -swc ../fullscreen-ane-android/bin/fullscreen-ane-android.swc ^
+ -platform Android-ARM -C android . ^
+ -platform Android-x86 -C android . ^
+ -platform default -C default . 
