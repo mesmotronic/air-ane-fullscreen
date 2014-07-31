0028557d3eaacc2f6a78305bcfe2a8aeb3ee4de3
Neil Rackett <neil.rackett@mesmotronic.com>
2014-07-31 21:21:12 +0100

    Removed XML declaration from extension.xml and re-built using Flex SDK 4.6

--- fullscreen-ane-build/build.cmd
+++ fullscreen-ane-build/build.cmd
@@ -1,9 +1,11 @@
 @echo off
 
+REM "C:\Program Files\Adobe\Adobe Flash Builder 4.7 (64 Bit)\sdks\4.6.0\bin\adt" ^
+
 adt ^
  -package ^
  -target ane ./AndroidFullScreen.ane extension.xml ^
- -swc ../fullscreen-ane-android/bin/fullscreen-ane-android.swc ^
+ -swc swc/fullscreen-ane-android.swc ^
  -platform Android-ARM -C android . ^
  -platform Android-x86 -C android . ^
  -platform default -C default . 
