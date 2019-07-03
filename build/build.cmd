8e3e102352b85b61ed007633d273453e15a35e2d
Neil Rackett <neil.rackett@mesmotronic.com>
2019-07-03 14:32:06 +0100

    Added support for Android-ARM64

--- build/build.cmd
+++ build/build.cmd
@@ -2,10 +2,11 @@
 
 xcopy ..\jar\bin\fullscreen-ane-jar.jar android /Y
 
-call adt ^
+call "C:\Program Files\Adobe\Adobe Flash Builder 4.7 (64 Bit)\sdks\4.16.1 AIR33\bin\adt" ^
  -package ^
  -target ane ./AndroidFullScreen.ane extension.xml ^
  -swc ../swc/bin/fullscreen-ane-swc.swc ^
  -platform Android-ARM -C android . ^
+ -platform Android-ARM64 -C android . ^
  -platform Android-x86 -C android . ^
  -platform default -C default . 
